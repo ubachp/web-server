29e14373c2dfe91170d5884e93b54cea7ecf1e94
Paulo Ubach <paulo.ubach@gmail.com>
2020-03-23 22:37:51 -0400

    feat: Start program as a service + TCP connection

---
+++ src/web-server/PWebServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace P.Web.Server
+{
+
+    public class PWebServer
+    {
+        private readonly TcpListener _listener;
+
+        public PWebServer()
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, 1337);
+            _listener = new TcpListener(ipEndPoint);
+        }
+        public bool Start()
+        {
+            Console.WriteLine("STARTED");
+            _listener.Start();
+            Task.Run(HandleConnection);
+            return true;
+        }
+
+        public Task HandleConnection()
+        {
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            // Enter the listening loop.
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also user server.AcceptSocket() here.
+                TcpClient client = _listener.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
+        }
+
+        public bool Stop()
+        {
+            Console.WriteLine("STOPPED");
+            _listener.Stop();
+            return true;
+        }
+    }
+}
---
+++ src/web-server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Topshelf;
+
+namespace P.Web.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<PWebServer>(s =>                                   
+                {
+                    s.ConstructUsing(name => new PWebServer());                
+                    s.WhenStarted( (server, host) => server.Start());                         
+                    s.WhenStopped( (server, host) => server.Stop());                          
+                });
+                x.RunAsLocalSystem();                                       
+
+                x.SetDescription("Paulo Web Server Implementation");                   
+                x.SetDisplayName("P-Web-Server");                                  
+                x.SetServiceName("PWebServer");
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
